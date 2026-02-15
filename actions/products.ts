44cd4e909a6afce61d3982f9865b7bc431c22297
Sayandeep <sdroy001@gmail.com>
2026-02-15 21:19:54 +0530

    added cart and stuff

---
+++ actions/products.ts
@@ -0,0 +1,14 @@
+'use server'
+import WooCommerceRestApi from "woocommerce-rest-ts-api";
+
+const WooCommerce = new WooCommerceRestApi({
+    url: "http://shoes.local/",
+    consumerKey: process.env.WC_CONSUMER_KEY as string,
+    consumerSecret: process.env.WC_CONSUMER_SECRET as string,
+    version: "wc/v3",
+})
+
+export const getProducts = async () => {
+    const products = await WooCommerce.get("products");
+    return products.data;
+}
